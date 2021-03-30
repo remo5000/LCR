24f848d98fa512b37670506c3f2877edcbb8a3eb
Vignesh Shankar <vigneshshankar@gmail.com>
2021-03-31 00:21:44 +0800

    Allow for >0 label size & find->ls

--- LCRIndexing/experiments/Index/UnboundedLCR/genQueries.sh
+++ LCRIndexing/experiments/Index/UnboundedLCR/genQueries.sh
@@ -1,17 +1,28 @@
 #!/bin/sh
-datasets=`find experiments/graphs/ | egrep '\.edge$' | sed 's:\.edge::g'`;
+
+# TODO set pattern here
+datasets=`ls experiments/graphs/real/stringFc.edge | sed 's:\.edge::g'`;
 for ds in $datasets;
 do
     dsp=`echo $ds | sed 's/.*\///'`;
     L=`cat ${ds}.edge | awk '{ if( $3 in used ){ } else { used[$3]=1; print $3 } }' | wc -l`;
+    # # TODO set number of labels here
+    # L1=1;
+    # L2=2;
+    # L3=2;
     L1=`expr $L / 4`;
     L2=`expr $L / 2`;
     L3=`expr $L - 2`;
 
+    tmp=`mktemp`
     if [ ! -f "${ds}.querylog" ];
     then
         echo "L=${L}";
-        echo "build/default/genQuery ${ds}.edge 3 1000 ${L1} ${L2} ${L3} > ${ds}.querylog";
-        build/default/genQuery ${ds}.edge 3 1000 ${L1} ${L2} ${L3} > ${ds}.querylog
+        echo "build/default/genQuery ${ds}.edge 3 1000 ${L1} ${L2} ${L3} > ${tmp}";
+        build/default/genQuery ${ds}.edge 3 1000 ${L1} ${L2} ${L3} > ${tmp}
+        echo "cp ${tmp} ${ds}.querylog";
+        cp ${tmp} ${ds}.querylog
+        echo "rm ${tmp}";
+        rm ${tmp}
     fi
 done;
