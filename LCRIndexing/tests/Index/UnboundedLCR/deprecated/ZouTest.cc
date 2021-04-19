cf18889d98325eb9b5b4b300018987253297f02a
Vignesh <vigneshshankar@gmail.com>
2021-04-19 17:51:37 +0800

    Update files (#22)
    * update readme
    
    * use py3 for gengraph
    
    * clear 'old' files
    
    * rm old wscript
    
    * rm BoundedLCR
    
    * move old files to deprecated/ folders
    
    * override ZouTest
    
    * delete files that are already in deprecated(?)
    
    * remove Zou from runExperiment
    
    * Remove deprecated stuff from wscript
    
    * Keep old zoutest

--- LCRIndexing/tests/Index/UnboundedLCR/deprecated/ZouTest.cc
+++ LCRIndexing/tests/Index/UnboundedLCR/deprecated/ZouTest.cc
@@ -1,5 +1,5 @@
-#include "../../Index/UnboundedLCR/Zou/Zou.cc"
-#include "../../Graph/DGraph.cc"
+#include "Index/UnboundedLCR/Zou.cc"
+#include "Graph/DGraph.cc"
 
 #include <string>
 
@@ -24,75 +24,118 @@ void test(bool a, int& t)
 void runQuery(Index* index, VertexID v, VertexID w, LabelSet ls, bool answer, int& t)
 {
     test(index->query(v,w,ls) == answer, t);
-    cout << "time=" << index->getLastQueryTime() << endl;
+    cout << "query(" << v << "," << w << "," << ls << ") took time(s)=" << index->getLastQueryTime() << endl;
 }
 
 int main(int argc, char *argv[])
 {
-    Graph* g1 = new DGraph("tests/graphs/ZouGraph.edge");
+    Graph* g1 = new DGraph("tests/graphs/Zoudummy.edge");
     int t = 1;
     Zou* zI = new Zou(g1);
 
+    cout << zI->toString() << endl;
+
+
+    return 1;
+
     //cout << zI->toString() << endl;
 
-    /*runQuery(zI, 0, 1, 1, true, t);
-    runQuery(zI, 0, 2, 2, true, t);
-    runQuery(zI, 0, 8, 2, true, t);
-    runQuery(zI, 0, 7, 5, true, t);
-    runQuery(zI, 4, 7, 1, true, t);
-    runQuery(zI, 4, 6, 2, true, t);
-    runQuery(zI, 1, 4, 4, true, t);
-    runQuery(zI, 12, 11, 3, true, t);
-    runQuery(zI, 12, 8, 2, true, t);
-
-    runQuery(zI, 2, 0, 9, true, t);
-    runQuery(zI, 3, 0, 3, true, t);
-    runQuery(zI, 12, 0, 6, true, t);
-    runQuery(zI, 12, 0, 34, true, t);
-
-    runQuery(zI, 0, 4, 2, false, t);
-    runQuery(zI, 4, 3, 7, false, t);
-    runQuery(zI, 12, 11, 1, false, t);
-    runQuery(zI, 4, 0, 34, false, t);
-    runQuery(zI, 5, 0, 20, false, t);
-    runQuery(zI, 8, 0, 5, false, t);
-    runQuery(zI, 10, 0, 9, false, t);
-    runQuery(zI, 11, 0, 17, false, t);
-
-    cout << "-------------" << endl;*/
-
-    g1 = new DGraph("tests/graphs/V30D3L8uni.edge");
+    g1 = new DGraph("tests/graphs/ZouGraph.edge");
     zI = new Zou(g1);
 
-    runQuery(zI, 5, 13, 72, true, t);
-    runQuery(zI, 6, 2, 68, true, t);
-    runQuery(zI, 10, 2, 130, true, t);
+    runQuery(zI,0,5,13,true,t);
+    runQuery(zI,0,6,21,true,t);
+    runQuery(zI,1,0,11,true,t);
+    runQuery(zI,1,2,19,true,t);
+    runQuery(zI,2,0,7,true,t);
+    runQuery(zI,2,1,7,true,t);
+    runQuery(zI,2,4,13,true,t);
+    runQuery(zI,2,5,7,true,t);
+    runQuery(zI,2,6,37,true,t);
+    runQuery(zI,3,10,35,true,t);
+    runQuery(zI,4,7,25,true,t);
+    runQuery(zI,5,6,7,true,t);
+    runQuery(zI,6,5,11,true,t);
+    runQuery(zI,6,9,7,true,t);
+    runQuery(zI,10,8,49,true,t);
+    runQuery(zI,12,4,21,true,t);
+    runQuery(zI,13,0,13,true,t);
+    runQuery(zI,13,0,14,true,t);
+    runQuery(zI,13,0,25,true,t);
+    runQuery(zI,13,3,11,true,t);
 
-    runQuery(zI, 11, 0, 129, false, t);
-    runQuery(zI, 12, 0, 33, false, t);
-    runQuery(zI, 13, 0, 65, false, t);
+    runQuery(zI,0,4,41,false,t);
+    runQuery(zI,1,5,25,false,t);
+    runQuery(zI,3,5,41,false,t);
+    runQuery(zI,4,0,19,false,t);
+    runQuery(zI,4,2,7,false,t);
+    runQuery(zI,5,0,11,false,t);
+    runQuery(zI,5,0,25,false,t);
+    runQuery(zI,6,0,19,false,t);
+    runQuery(zI,7,2,41,false,t);
+    runQuery(zI,9,0,37,false,t);
+    runQuery(zI,9,2,13,false,t);
+    runQuery(zI,10,0,42,false,t);
+    runQuery(zI,10,0,49,false,t);
+    runQuery(zI,10,1,37,false,t);
+    runQuery(zI,11,0,38,false,t);
+    runQuery(zI,11,1,49,false,t);
+    runQuery(zI,11,2,25,false,t);
+    runQuery(zI,13,5,26,false,t);
+    runQuery(zI,15,0,11,false,t);
+    runQuery(zI,16,0,49,false,t);
+
+    cout << "-------------" << endl;
 
     g1 = new DGraph("tests/graphs/V100D5L8uni.edge");
     zI = new Zou(g1);
 
-    runQuery(zI, 44, 0, 344, true, t);
-    runQuery(zI, 48, 1, 337, true, t);
-    runQuery(zI, 77, 1, 120, true, t);
-    runQuery(zI, 98, 1, 275, true, t);
+    runQuery(zI,4,0,304,true,t);
+    runQuery(zI,10,1,28,true,t);
+    runQuery(zI,11,1,21,true,t);
+    runQuery(zI,21,0,224,true,t);
+    runQuery(zI,27,3,290,true,t);
+    runQuery(zI,53,0,193,true,t);
+    runQuery(zI,59,0,26,true,t);
+    runQuery(zI,62,1,131,true,t);
+    runQuery(zI,64,3,164,true,t);
+    runQuery(zI,65,0,140,true,t);
+    runQuery(zI,69,0,112,true,t);
+    runQuery(zI,71,4,140,true,t);
+    runQuery(zI,75,1,140,true,t);
+    runQuery(zI,79,0,97,true,t);
+    runQuery(zI,80,7,176,true,t);
+    runQuery(zI,81,0,49,true,t);
+    runQuery(zI,89,0,42,true,t);
+    runQuery(zI,94,1,97,true,t);
+    runQuery(zI,97,0,25,true,t);
+    runQuery(zI,98,1,100,true,t);
 
-    runQuery(zI, 75, 6, 141, false, t);
-    runQuery(zI, 83, 6, 393, false, t);
-    runQuery(zI, 88, 6, 269, false, t);
-    runQuery(zI, 97, 6, 393, false, t);
+    runQuery(zI,0,1,265,false,t);
+    runQuery(zI,6,3,84,false,t);
+    runQuery(zI,8,1,296,false,t);
+    runQuery(zI,16,5,266,false,t);
+    runQuery(zI,22,1,352,false,t);
+    runQuery(zI,24,1,352,false,t);
+    runQuery(zI,27,3,26,false,t);
+    runQuery(zI,39,3,324,false,t);
+    runQuery(zI,48,3,22,false,t);
+    runQuery(zI,58,0,328,false,t);
+    runQuery(zI,60,1,352,false,t);
+    runQuery(zI,70,2,274,false,t);
+    runQuery(zI,73,4,352,false,t);
+    runQuery(zI,75,6,268,false,t);
+    runQuery(zI,77,0,448,false,t);
+    runQuery(zI,84,3,146,false,t);
+    runQuery(zI,84,3,386,false,t);
+    runQuery(zI,90,3,259,false,t);
+    runQuery(zI,97,3,26,false,t);
+    runQuery(zI,99,1,392,false,t);
 
-    g1 = new DGraph("experiments/graphs/synthetic/PA/uni/graphs/V1kD2L8uni.edge");
+    g1 = new DGraph("experiments/graphs/real/robots/robots.edge");
     zI = new Zou(g1);
 
-    g1 = new DGraph("experiments/graphs/synthetic/PA/uni/graphs/V1kD5L8uni.edge");
-    zI = new Zou(g1);
-
-    g1 = new DGraph("experiments/graphs/synthetic/PA/uni/graphs/V5kD2L8uni.edge");
-    zI = new Zou(g1);
+    runQuery(zI,101,17,6,true,t);
 
     return 0;
 }
--- README.md
+++ README.md
@@ -1,60 +1,45 @@
 ## Synopsis
 
-This is the code + thesis for my graduation project at the Technical University of Eindhoven, July 2016.
+This is a fork of https://github.com/DeLaChance/LCR .
+Please refer to `README.md.old` if you wish to cite the original author.
 
-This project contains several methods for answering label-constraint reachability queries (LCR-queries). Our major contribution is the LandmarkedIndex-method. 
-
-To cite this work, please use the following reference.
-
-Landmark indexing for evaluation of label-constrained reachability queries. Lucien Valstar, George Fletcher, Yuichi Yoshida. SIGMOD 2017. http://doi.acm.org/10.1145/3035918.3035955
-
-For further details, please see the following work on which this is based.
-
-Landmark indexing for scalable evaluation of label-constrained reachability queries. Lucien Valstar.  MSc thesis, Eindhoven University of Technology, 2016. https://pure.tue.nl/ws/files/46945317/855447-1.pdf
-
-
+This project contains several methods for answering label-constraint reachability queries (LCR-queries).
+The major contribution of this fork is the BackboneIndex.
 
 ## Code Example
 
-The main part of the code is written in C++ and there are scripts in bash/sh and Python (2.7).
+The main part of the code is written in C++ and there are scripts in bash/sh and python3.
 
-The C++-part has a definition of a labelled graph (Graph), a set of indices to answer LCR-queries (Index/Unbounded), a number of tests (tests/Index/Unbounded) and to run the experiments or generate queries for the experiments (experiments/Index/Unbounded).
+The C++-part has:
+- a definition of a labelled graph (Graph)
+- a set of indices to answer LCR-queries (Index/Unbounded)
+- a number of tests (tests/Index/Unbounded) 
+- scripts to run the experiments or generate queries for the experiments (experiments/Index/Unbounded).
 
-The Python-part consists of a script to generate a synthetic graph under a given model with a specified number of vertices.
-
-For example
-
-```python
-python2.7 genGraph.py 1000 5 8 exp pa
-```
-
-generates a graph with 1000 vertices and roughly 5000 edges under the 'Preferential-Attachment' model. Any edge has one of 8 possible labels. The distribution of the edge labels is an exponential one.
+The Python/sh parts consist of:
+- a script to generate a synthetic graph under a given model with a specified number of vertices. (datagen)
+- a script to download real graphs and label them randomly (experiments...install.sh)
 
 ## Installation
 
-You need g++ (>=4.5) and python2.7.
+For C++ you need:
+- g++ (>=9.3.0) or clang (>=12.0.0)
+- OpenMP libraries
+
+For python3 you need:
+- snap-stanford
+
+## Build
 
 The code can be built by running:
 
 ```sh
 cd LCRIndexing
-sh ./rebuild
+sh ./build.sh
 ```
 
-on Mac or Linux. 
+You can rebuild using `rebuild.sh`.
 
-For Windows you should first remove the directories: build and .waf* (where * is an arbitary sequence). Then run:
+## Experiment Driver
 
-```python
-python2.7 waf configure
-python2.7 waf build
-```
-
-## Contributors
-
-Myself. Lucien Valstar: l + d + j + (last name in lower case letters) at gmail.com
-
-My supervisors. George Fletcher: g dot h dot l dot (last name in lower case letters) at tue.nl
-
-Yuichi Yoshida: y dot (last name in lower case letters) at gmail.com
-
+To run experiments, we use `runExperiment.sh` as an entry point to `runExperiment.cc`.
